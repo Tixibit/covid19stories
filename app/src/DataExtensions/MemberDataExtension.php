227edb8789c1acf94aa909487cdea04ddbe01635
Lamin Barrow <laminbarrow@gmail.com>
2020-05-12 08:28:18 +0000

    Added a member grouped. Users will be added to this group after they sign up

--- app/src/DataExtensions/MemberDataExtension.php
+++ app/src/DataExtensions/MemberDataExtension.php
@@ -2,7 +2,9 @@
 namespace App;
 
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\ORM\DB;
 use SilverStripe\ORM\FieldType\DBVarchar;
+use SilverStripe\Security\Group;
 
 class MemberDataExtension extends DataExtension
 {
@@ -13,4 +15,26 @@ class MemberDataExtension extends DataExtension
     private static $has_many = [
         'COVIDStories' => COVIDStory::class,
     ];
+
+    /**
+     * Add default records to database. This function is called whenever the
+     * database is built, after the database tables have all been created. Overload
+     * this to add default records when the database is built, but make sure you
+     * call parent::requireDefaultRecords().
+     *
+     * @uses DataExtension->requireDefaultRecords()
+     */
+    public function requireDefaultRecords()
+    {
+        // Add a member group if one does not already exist
+        $memberGroup = Group::get()->filter('Code', 'member')->first();
+        if (!$memberGroup) {
+            $memberGroup = new Group();
+            $memberGroup->Code = 'member';
+            $memberGroup->Title = 'Members';
+            $memberGroup->Sort = 1;
+            $memberGroup->write();
+            DB::alteration_message('Member Group Created', 'created');
+        }
+    }
 }
